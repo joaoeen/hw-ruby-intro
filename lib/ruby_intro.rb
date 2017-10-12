4e9dfdce7d0cca5f48bdef8240685a0f5d543bd5
João Marcos de Aguiar <joao.een@gmail.com>
2017-10-12 19:11:26 +0000

    first parts of the homework

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,12 +2,24 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(array)
+  result = 0
+  array.each do |value|
+    result += value
+  end
+  return result
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(arr)
+  result = 0
+  arr.sort!
+  arr.reverse!
+  if arr.length > 1 then
+    result = arr[0] + arr[1]
+  elsif arr.length >0 
+    result = arr[0]
+  end
+  result
 end
 
 def sum_to_n? arr, n
